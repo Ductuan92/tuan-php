9745198e5879f7d5ae3a5b90e1232f490db456c2
Tuan <ductuan.tran@nfq.asia>
2022-05-18 06:05:27 +0700

    (fix) remove deleted files

--- designPattern/index.php
+++ designPattern/index.php
@@ -1,1 +1,19 @@
 <?php
+
+use MyApp\Facade\ProductOrderFacade;
+use MyApp\Facade\ProductFacade;
+use MyApp\Decorator\SnackDecorator;
+use MyApp\Decorator\BeerDecorator;
+use MyApp\Decorator\MainFoodDecorator;
+
+require_once realpath("vendor/autoload.php");
+
+
+//code to test facade
+$dress = new ProductFacade("111","red_dress","20usd");
+$dressOrder = new ProductOrderFacade($dress);
+echo $dressOrder->generateOrder();
+
+$food =  new MainFoodDecorator();
+$beer =  new BeerDecorator($food);
+echo $beer->addFood();
--- designPattern/src/designPatternFactory/FactoryCustomer.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-interface FactoryCustomer{
-    const Clothes = 1;
-    const Noodles = 2;
-    const Beverage = 3;
-    public function deliverProduct();
-}
